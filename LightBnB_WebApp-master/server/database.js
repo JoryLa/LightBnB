bfbec2de362c05811fd9f44edc22cf809a609e6d
JorLo <jory.laurin@gmail.com>
2022-06-02 19:29:43 -0700

    Creates property and adss to database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -162,15 +162,43 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-    return pool
-    .query(`INSERT INTO properties (name, email, password) 
-      VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
-    .then((res) => {
-      const property = res.rows[0];
-      return property;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  const queryString = 
+    `INSERT INTO properties(
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms
+      ) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) 
+      RETURNING *;`;
+    const values = [
+      property.owner_id,
+      property.title,
+      property.description,
+      property.thumbnail_photo_url,
+      property.cover_photo_url,
+      property.cost_per_night,
+      property.street,
+      property.city,
+      property.province,
+      property.post_code,
+      property.country,
+      property.parking_spaces,
+      property.number_of_bathrooms,
+      property.number_of_bedrooms,
+    ];
+    
+    return pool.query(queryString, values).then((res) => res.rows);
 }
 exports.addProperty = addProperty;
+
